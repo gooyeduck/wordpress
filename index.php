cd01780f35a9e14421121612ef8969d300a24546
gooyeduck <dorhubs@gmail.com>
2023-08-20 09:34:43 +0600

    feat: database connection with pdo

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+class Database {
+	public function query() {
+	}
+}
+
+$db = new Database();
+
+$host     = 'localhost';
+$db       = 'WordPress';
+$user     = 'root';
+$password = '';
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+try {
+	$pdo = new PDO( $dsn, $user, $password );
+
+	if ( $pdo ) {
+		echo "Connected to the $db database successfully!";
+	}
+} catch ( PDOException $e ) {
+	echo $e->getMessage();
+	die();
+}
+
+$statement = $pdo->prepare( 'SELECT * FROM posts' );
+$statement->execute();
+$posts = $statement->fetchAll( PDO::FETCH_ASSOC );
+
+foreach ( $posts as $post ) {
+	echo "<li>{$post['title']}</li>";
+}
